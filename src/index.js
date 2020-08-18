6451cd8180b43161806ca62989eebb0c8f38becd
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-08-18 10:24:02 +0200

    strona glowna

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/main.scss";
-
 import Header from "./start";
+import { motion } from "framer-motion";
 
 const App = () => {
   return (

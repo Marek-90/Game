ac7ff3cfe980e0f33270bc9501e5790fa479e5de
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-08-25 18:11:46 +0200

    timer

--- src/timer.jsx
+++ src/timer.jsx
@@ -2,15 +2,28 @@ import React, { useState, useEffect } from "react";
 
 const Timer = () => {
   const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+
+  useEffect(() => {
+    if (sec === 60) {
+      setMin((prevState) => prevState + 1);
+      setSec(0);
+    }
+  }, [sec]);
 
   useEffect(() => {
     const time = setInterval(() => {
       setSec((prevState) => prevState + 1);
     }, 1000);
-    return () => clearInterval(time);
-  });
 
-  return <div>{sec}</div>;
+    return () => clearInterval(time);
+  }, []);
+
+  return (
+    <div>
+      {min} min {sec} sec
+    </div>
+  );
 };
 
 export default Timer;
